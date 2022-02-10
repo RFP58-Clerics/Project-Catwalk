e60aedc697ca92658336eb0244c72b14516447df
Anthony Blelloch <anthony.be510@gmail.com>
2022-02-10 14:23:44 -0800

    reorganize App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-
+import ProductDetail from './ProductDetail/ProductDetail.jsx';
+import RelatedApp from './RelatedItems/RelatedApp.jsx';
+import RARApp from './RatingsAndReview/RARApp.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 
-import ProductDetail from './ProductDetail/ProductDetail.jsx';
 
-import RARApp from './RatingsAndReview/RARApp.jsx';
-
-
-const App = (props) => (
-  <div>
-    <h1>Team Cleric!!</h1>
-<<<<<<< HEAD
-    <QuestionsAndAnswer />
-    <RARApp />
-    <Productdetail />
-    <RelatedApp />
-
-=======
-    <QuestionsAndAnswers />
-    <RARApp />
-    <ProductDetail />
->>>>>>> f282555b7ed61abf24eb6d5ae6e260084d3bf358
-  </div>
-)
+const App = (props) => {
+  return (
+    <div>
+      <h1>Team Cleric!!</h1>
+      <ProductDetail />
+      <RelatedApp />
+      <RARApp />
+      <QuestionsAndAnswers />
+    </div>
+  )
+}
 
 export default App;
--- src/Components/RelatedItems/RelatedApp.jsx
+++ src/Components/RelatedItems/RelatedApp.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const RelatedApp = () {
-
+  return (
+    <div>Anthony is the best</div>
+  )
 }
 
 export default RelatedApp;
