56db14cbb7bf0ec59cad02f470743147ded166f6
Karli Thomas <karliholthouse@gmail.com>
2022-02-10 11:07:02 -0800

    Added Ratings and Reviews component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+
+import RARApp from 'RARApp.jsx';
+
 const App = (props) => (
   <div>
     <h1>Team Cleric!!</h1>
+
+
+    <RARApp />
   </div>
 )
 
---
+++ src/Components/RatingsAndReview/RARApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RARApp = (props) => {
+  return (
+    <div>This is Ratings and Review</div>
+  );
+}
+
+export default RARApp;
