0d785ca85288100539f84bcafa35243e9598581f
Lorene Chew <lorene.chew@gmail.com>
2022-02-10 11:05:54 -0800

    created Q&A jsx file

---
+++ src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuestionsAndAnswer = () => {
+  return (
+    <div>
+      <h3>Questions & Answers</h3>
+    </div>
+  )
+}
+
+export default QuestionsAndAnswer;
