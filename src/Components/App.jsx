7abb51cbfb93c9c162cf67444bc55dc68802291c
Lorene Chew <lorene.chew@gmail.com>
2022-02-10 11:37:27 -0800

    updated app.jsx to include q&a component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import QuestionsAndAnswer from './QuestionsAndAnswer';
 
 const App = (props) => (
   <div>
     <h1>Team Cleric!!</h1>
+    <QuestionsAndAnswer />
   </div>
 )
 
