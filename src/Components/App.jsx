86617da5ba2e0089baa59b25adf1cd1f563d9922
Christian Gorlewski <Cgorlewski@gmail.com>
2022-02-10 14:34:02 -0800

    Committing so I can pull

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
-
 import ProductDetail from './ProductDetail/ProductDetail.jsx';
-
 import RARApp from './RatingsAndReview/RARApp.jsx';
 
 
 const App = (props) => (
   <div>
     <h1>Team Cleric!!</h1>
-    <QuestionsAndAnswers />
-    <RARApp />
     <ProductDetail />
+    <RARApp />
+    <QuestionsAndAnswers />
   </div>
 )
-
 export default App;
