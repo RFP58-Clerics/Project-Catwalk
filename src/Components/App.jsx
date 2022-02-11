fd663ffca7d5f65a9c11371d03bae2e9a42d3b5d
Christian Gorlewski <Cgorlewski@gmail.com>
2022-02-10 16:12:26 -0800

    Adding dist/main and stuff

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
-import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
-import ProductDetail from './ProductDetail/ProductDetail.jsx';
-=======
 import ProductDetail from './ProductDetail/ProductDetail.jsx';
 import RelatedApp from './RelatedItems/RelatedApp.jsx';
->>>>>>> fafb034b19e218b894e27e035b0f87b701c705de
 import RARApp from './RatingsAndReview/RARApp.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 
 
-<<<<<<< HEAD
-const App = (props) => (
-  <div>
-    <h1>Team Cleric!!</h1>
-    <ProductDetail />
-    <RARApp />
-    <QuestionsAndAnswers />
-  </div>
-)
-=======
 const App = (props) => {
   return (
     <div>
@@ -32,5 +17,4 @@ const App = (props) => {
   )
 }
 
->>>>>>> fafb034b19e218b894e27e035b0f87b701c705de
 export default App;
