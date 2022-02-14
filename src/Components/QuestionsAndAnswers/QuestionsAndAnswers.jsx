77a21613b6cdbae1ea90aa5ce87ac6ae308161b8
Lorene Chew <lorene.chew@gmail.com>
2022-02-14 12:26:49 -0800

    added search component for question&answers

--- src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Search from './Search.jsx';
 
 const QuestionsAndAnswers = () => {
   return (
     <div>
       <h3>Questions & Answers</h3>
+      <Search />
     </div>
   )
 }
