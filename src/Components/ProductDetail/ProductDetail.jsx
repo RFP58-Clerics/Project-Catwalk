7967e1dec325481df9403ccc5ae35d9973915d9a
Christian Gorlewski <Cgorlewski@gmail.com>
2022-02-14 15:02:40 -0800

    Building out ProductDetail

--- src/Components/ProductDetail/ProductDetail.jsx
+++ src/Components/ProductDetail/ProductDetail.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 
 const ProductDetail = (props) => (
-  <div>
-    idk lol
-  </div>
+  <>
+    <div class='logo'>This is the logo</div>
+    <div class='search'>This is the search bar</div>
+    <div class='image'>This is the product image</div>
+    <div class='product_name'>This is the product name</div>
+  </>
 )
-
 
 export default ProductDetail;
