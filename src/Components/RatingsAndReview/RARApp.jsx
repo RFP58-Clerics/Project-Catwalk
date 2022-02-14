e9618ffe9487ec5d512a22a9452a276ebec9168a
Karli Thomas <karliholthouse@gmail.com>
2022-02-14 09:11:01 -0800

    Grabbing info from other karli branch

--- src/Components/RatingsAndReview/RARApp.jsx
+++ src/Components/RatingsAndReview/RARApp.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
-const RARApp = (props) => {
-  return (
-    <div>This is Ratings and Review</div>
-  );
+
+class RARApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: []
+    }
+  }
+
+
+  render() {
+    return (
+      <div>This is Ratings and Review</div>
+    )
+  }
 }
 
 export default RARApp;
