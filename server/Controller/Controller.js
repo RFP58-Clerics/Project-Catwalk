7110e8652491305961415037ebb25f4491e57dee
Karli Thomas <karliholthouse@gmail.com>
2022-02-15 11:27:20 -0800

    Updated some eslint errors

--- server/Controller/Controller.js
+++ server/Controller/Controller.js
@@ -4,41 +4,14 @@ const axios = require('axios');
 
 // Set up axios defaults so we don't have to pass in the base url, token, etc. every time
 axios.defaults.baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
-axios.defaults.headers.common['Authorization'] = TOKEN.TOKEN;
-//axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.common.Authorization = TOKEN.TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 module.exports = {
-  getProducts: () => {
-    return axios.get("products");
-    // .then((result) => {
-    //   callback(result.data);
-    //   })
-    // .catch(err => {
-    //   callback(null, err);
-    // })
-  },
-  getReviews: (productId) => {
-    return axios.get(`reviews?product_id=${productId}`);
-  },
+  getProducts: () => axios.get('products'),
+  getReviews: (productId) => axios.get(`reviews?product_id=${productId}`),
 
-  getQuestions: (productId) => {
-    return axios.get(`qa/questions/?product_id=${productId}`);
-  },
+  getQuestions: (productId) => axios.get(`qa/questions/?product_id=${productId}`),
 
-  getMetaData: (productId) => {
-    return axios.get(`reviews/meta?product_id=${productId}`)
-  },
-}
-
-// },
-
-  // post: (req, res) => {
-  //   var params = req.body.id;
-  //   Model.model.save(params, (err, result) => {
-  //     if (err) {
-  //       res.status(500).send(err);
-  //     } else {
-  //       res.status(201).send(result);
-  //     }
-  //   })
-  // }
+  getMetaData: (productId) => axios.get(`reviews/meta?product_id=${productId}`),
+};
--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ app.use(express.static('dist'));
 app.get('/products', (req, res) => {
   Controller.getProducts()
     .then(result => {
-      console.log(result.data),
-      res.send(result.data)
+      res.send(result.data);
     })
     .catch(err => {
       console.log(err);
@@ -20,13 +19,13 @@ app.get('/products', (req, res) => {
 });
 
 app.get('/products/:productId/reviews', (req, res) => {
-  let productId = req.params.productId;
+  const { productId } = req.params;
   Controller.getReviews(productId)
-    .then(result => res.send(result.data.results))
-    .catch(err => {
+    .then((result) => res.send(result.data.results))
+    .catch((err) => {
       console.log(err);
       res.sendStatus(500);
-    })
+    });
 });
 
 app.get('/qa/questions/:product_id', (req, res) => {
@@ -36,24 +35,22 @@ app.get('/qa/questions/:product_id', (req, res) => {
     .catch(err => {
       // console.log(err);
       res.sendStatus(500);
-    })
+    });
 });
 
 app.get('/reviews/meta/:productId', (req, res) => {
-  let productId = req.params.productId;
+  const { productId } = req.params;
   console.log(productId);
   Controller.getMetaData(productId)
-    .then(result => res.json(result.data))
-    .catch(err => {
+    .then((result) => res.json(result.data))
+    .catch((err) => {
       console.log(err);
       res.sendStatus(500);
-    })
+    });
 });
-
 
 // app.post('/products', Controller.controller.post);
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
