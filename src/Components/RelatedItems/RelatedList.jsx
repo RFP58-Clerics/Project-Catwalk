ee3537bad263b80c3a7570eeccf908e71bd89e17
Anthony Blelloch <anthony.be510@gmail.com>
2022-02-16 14:22:44 -0800

    Complete functionality of related carousel

--- src/Components/RelatedItems/RelatedList.jsx
+++ src/Components/RelatedItems/RelatedList.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RelatedCards from './RelatedCards.jsx';
 import './styles.css';
 
 const RelatedList = (props) => {
+  const [index, setIndex] = useState(0);
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= props.related.length) {
+      newIndex = props.related.length - 1;
+    }
+    setIndex(newIndex);
+  }
   return (
-    <div className='related-bo'>
-      <button className='carousel-button carousel-button--left'>
+    <div className='outer-container'>
+      {index !== 0 &&
+        <button className='carousel-button carousel-button-left' onClick={() => {updateIndex(index - 1)}}>
         </button>
+        }
         <div className='carousel'>
-          <div className='related-box'>
+          <div className='related-box' style={{transform: `translateX(-${index * 50}%)`}} >
             {props.related.map((item, index) => {
-              return <RelatedCards item={item} key={index} pos={index + 1} />
+              return <RelatedCards item={item} key={index} pos={index + 1} width={{width: '100%'}} total={props.related.length}/>
             })}
           </div>
         </div>
-      <button className='carousel-button carousel-button--right'>
-        </button>
+        {index !== props.related.length - 1 &&
+          <button className='carousel-button carousel-button-right' onClick={() => {updateIndex(index + 1)}}>
+          </button>
+          }
     </div>
   )
 }
