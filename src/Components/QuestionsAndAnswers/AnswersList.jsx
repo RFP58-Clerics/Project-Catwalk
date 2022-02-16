d345add5ae612548e118a938a2c2cd3b39f88d70
Christian Gorlewski <Cgorlewski@gmail.com>
2022-02-15 19:50:25 -0800

    Pushing to pull

--- src/Components/QuestionsAndAnswers/AnswersList.jsx
+++ src/Components/QuestionsAndAnswers/AnswersList.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import AnswersListItem from './AnswersListItem.jsx';
 
 const AnswersList = ({answers}) => {
-  var answersBody = [];
-  if (answers) {
-    Object.values(answers).forEach(answer => {
-      answersBody.push(answer.body);
-    })
-    return answersBody;
-  }
-
   return (
     <div>
       {answers ? Object.values(answers).map((answer, i) => <AnswersListItem answer={answer.body} key={i}/>) : null }
