401447f690c0a263a2331f50c8fc747c3f19552a
Christian Gorlewski <Cgorlewski@gmail.com>
2022-02-15 20:19:07 -0800

    Added a test

--- src/Components/ProductDetail/ProductDetail.test.js
+++ src/Components/ProductDetail/ProductDetail.test.js
@@ -1,51 +1,16 @@
-// import dependencies
-import React from 'react';
-// import API mocking utilities from Mock Service Worker
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-// import react-testing methods
-import {render, fireEvent, waitFor, screen} from '@testing-library/react';
-// add custom jest matchers from jest-dom
-import {
-  test, beforeAll, afterEach, afterAll,
-} from '@testing-library/jest-dom';
-// the component to test
-import RARApp from './RARApp.jsx';
+import * as React from 'react'
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+import {render, fireEvent, screen, waitFor} from '@testing-library/react'
+// import userEvent from '@testing-library/user-event'
+// import '@testing-library/jest-dom/extend-expect'
+import ProductDetail from './ProductDetail.jsx'
+// import {expect} from 'jest'
+// import RelatedList from './RelatedList.jsx'
+import '@testing-library/jest-dom'
 
-// declare which API requests to mock
-const server = setupServer(
-  // capture "GET /greeting" requests
-  rest.get('/greeting', (req, res, ctx) =>
-    // respond using a mocked JSON body
-    res(ctx.json({ greeting: 'hello there' }))),
-);
-
-// establish API mocking before all tests
-beforeAll(() => server.listen());
-// reset any request handlers that are declared as a part of our tests
-// (i.e. for testing one-time error scenarios)
-afterEach(() => server.resetHandlers());
-// clean up once the tests are done
-afterAll(() => server.close());
-
-test('displays widget title on page load', async () => {
-  // Arrange
-  render(<RARApp url="/reviews" />);
-  // Act
-  fireEvent.click(screen.getByText('Ratings And Reviews'));
-
-  await waitFor(() => screen.getByRole('heading'));
-  // Assert
-});
-
-// ...
-
-test('handlers server error', async () => {
-  server.use(
-    // override the initial "GET /reviews" request handler
-    // to return a 500 Server Error
-    rest.get('/reviews', (req, res, ctx) => res(ctx.status(500))),
-  );
-
-  // ...
-});
+test('expect ProductDetail to render', () => {
+  const testMessage = 'this is a test';
+  render(<ProductDetail>{testMessage}</ProductDetail>)
+  expect(screen.getByText('Overview')).toBeInTheDocument()
+})
--- src/Components/RatingsAndReview/RARApp.test.js
+++ src/Components/RatingsAndReview/RARApp.test.js
@@ -1,23 +1,31 @@
+import 'babel-polyfill';
 // import dependencies
 import React from 'react';
 // import API mocking utilities from Mock Service Worker
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-// import react-testing methods
-import {render, fireEvent, waitFor, screen} from '@testing-library/react';
-// add custom jest matchers from jest-dom
 import {
-  test, beforeAll, afterEach, afterAll,
-} from '@testing-library/jest-dom';
+  describe, expect, test, beforeAll, afterAll, afterEach,
+} from '@jest/globals';
+// import react-testing methods
+import {
+  render, fireEvent, waitFor, screen,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+// // add custom jest matchers from jest-dom
+// import {
+//   test, beforeAll, afterEach, afterAll,
+// } from '@testing-library/jest-dom';
 // the component to test
 import RARApp from './RARApp.jsx';
 
 // declare which API requests to mock
 const server = setupServer(
   // capture "GET /greeting" requests
-  rest.get('/greeting', (req, res, ctx) =>
-    // respond using a mocked JSON body
-    res(ctx.json({ greeting: 'hello there' }))),
+  rest.get('/reviews/meta/*', (req, res, ctx) => res(
+    ctx.status(200),
+    ctx.json({ greeting: 'hello there' }),
+  )),
 );
 
 // establish API mocking before all tests
@@ -29,10 +37,23 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 test('displays widget title on page load', async () => {
+  // Fake Product
+  const product = {
+    id: 40344,
+    campus: 'hr-rfp',
+    name: 'Camo Onesie',
+    slogan: 'Blend in to your crowd',
+    description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+    category: 'Jackets',
+    default_price: '140.00',
+    created_at: '2021-08-13T14:38:44.509Z',
+    updated_at: '2021-08-13T14:38:44.509Z',
+  };
+
   // Arrange
-  render(<RARApp url="/reviews" />);
+  render(<RARApp product={product}/>);
   // Act
-  fireEvent.click(screen.getByText('Ratings And Reviews'));
+  fireEvent.click(screen.getByText('Ratings and Reviews'));
 
   await waitFor(() => screen.getByRole('heading'));
   // Assert
@@ -40,12 +61,12 @@ test('displays widget title on page load', async () => {
 
 // ...
 
-test('handlers server error', async () => {
-  server.use(
-    // override the initial "GET /reviews" request handler
-    // to return a 500 Server Error
-    rest.get('/reviews', (req, res, ctx) => res(ctx.status(500))),
-  );
+// test('handlers server error', async () => {
+//   server.use(
+//     // override the initial 'GET /reviews" request handler
+//     // to return a 500 Server Error
+//     rest.get('/reviews', (req, res, ctx) => res(ctx.status(500))),
+//   );
 
-  // ...
-});
+//   // ...
+// });
