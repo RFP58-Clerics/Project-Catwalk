c68f6687ed050c065f630f38ee7dea0cc3c1252f
Lorene Chew <lorene.chew@gmail.com>
2022-02-15 19:54:27 -0800

    added testing file

---
+++ src/Components/QuestionsAndAnswers/qa.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import QuestionsAndAnswers from './QuestionsAndAnswers';
+
+const server = setupServer(
+  rest.get('/greeting', (req, res, ctx) => {
+    return res(ctx.json({greeting: 'hello there'}))
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('loads and displays Questions & Answers on page', async () => {
+  render(<QuestionsAndAnswers url="/qa/questions" />)
+
+  fireEvent.click(screen.getByText('Questions & Answers'))
+
+  await waitFor(() => screen.getByRole('heading'))
+
+  expect(screen.getByRole('heading')).toHaveTextContent('hello there')
+  expect(screen.getByRole('button')).toBeDisabled()
+})
+
+test('handles server error', async () => {
+  server.use(
+    rest.get('/qa/questions', (req, res, ctx) => {
+      return res(ctx.status(500))
+    }),
+  )
+
+  render(<QuestionsAndAnswers url="/qa/questions" />)
+
+  fireEvent.click(screen.getByText('Load Greeting'))
+
+  await waitFor(() => screen.getByRole('alert'))
+
+  expect(screen.getByRole('alert')).toHaveTextContent('Oops, failed to fetch!')
+  expect(screen.getByRole('button')).not.toBeDisabled()
+})
