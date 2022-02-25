c59566ce07e71f10af02b401a24edb1d5c8f1136
Christian Gorlewski <cgorlewski@gmail.com>
2022-02-24 22:42:16 -0800

    CSS overhaul

--- src/Components/Overview/Overview.jsx
+++ src/Components/Overview/Overview.jsx
@@ -56,7 +56,7 @@ class Overview extends React.Component {
         this.setState({ styles: results.data });
         this.setState({ currentPhoto: results.data.results[0].photos[0].url });
         this.setState({ skus: _.map(this.state.currentStyle.skus) });
-        let newQty = [];
+        const newQty = [];
         for (let i = 1; i < this.state.skus[0].quantity + 1; i++) {
           newQty.push(i);
         }
@@ -113,7 +113,7 @@ class Overview extends React.Component {
     e.preventDefault();
     this.setState({ currentSize: JSON.parse(e.target.value).size });
     let arr = [];
-    let newQty = JSON.parse(e.target.value).quantity;
+    const newQty = JSON.parse(e.target.value).quantity;
     for (let i = 1; i <= newQty; i++) {
       arr.push(i);
       this.setState({ qty: arr });
@@ -127,78 +127,87 @@ class Overview extends React.Component {
   }
 
   enlargeImg() {
-    const img = document.getElementById("img1");
-    img.style.transform = "scale(1.5)";
-    img.style.transition = "transform 0.25s ease";
-    const button = document.getElementById("button1");
+    const img = document.getElementById('img1');
+    img.style.transform = 'scale(1.5)';
+    img.style.transition = 'transform 0.25s ease';
+    const button = document.getElementById('button1');
     button.style.display = 'inline';
   }
 
   resetImg() {
-    const img = document.getElementById("img1");
-    img.style.transform = "scale(1)";
-    img.style.transition = "transform 0.25s ease";
-    const button = document.getElementById("button1");
+    const img = document.getElementById('img1');
+    img.style.transform = 'scale(1)';
+    img.style.transition = 'transform 0.25s ease';
+    const button = document.getElementById('button1');
     button.style.display = 'none';
   }
 
   render() {
     return this.state.styles.length === 0 ? null : (
       <div className="overview">
-        <img className="currentPhoto" id="img1" src={this.state.currentPhoto} alt="main style" onClick={this.enlargeImg} />
-        <button className="resetImgButton" id="button1" onClick={this.resetImg}>X</button>
-        <div className="productThumbnails">
-          {this.state.currentStyle ? this.state.currentStyle.photos.map((photo, index) => (
-            <>
-              <ProductPhotos
-                photo={photo}
-                key={index}
-                changePhoto={this.changePhoto}
-              />
-              <br />
-              <br />
-            </>
-          )) : null}
+        <div className="imgAndButton">
+          <button className="resetImgButton" id="button1" onClick={this.resetImg}>X</button>
+          <img className="currentPhoto" id="img1" src={this.state.currentPhoto} alt="main style" onClick={this.enlargeImg} />
+          <div className="thumbnailWrapper">
+            <div className="productThumbnails">
+              {this.state.currentStyle ? this.state.currentStyle.photos.map((photo, index) => (
+                <ProductPhotos
+                  photo={photo}
+                  key={index}
+                  changePhoto={this.changePhoto}
+                />
+              )) : null}
+            </div>
+          </div>
         </div>
-        <div className="productDetailsContainer">
+        <div className="rightSide">
+          <div className="productDetailsContainer">
             <ProductDetails
               product={this.props.product}
               productPrice={this.state.currentStyle.original_price}
               salePrice={this.state.currentStyle.sale_price}
               reviews={this.state.reviews}
             />
+          </div>
+          <div className="stylesContainer">
+            {this.state.styles.results.map((style, index) => (
+              <MoreStyles
+                style={style}
+                key={index}
+                changeStyle={this.changeStyle}
+              />
+            ))}
+          </div>
+          <div className="cartGang">
+            <select className="selectSize" onChange={this.changeSize}>
+              <option selected>Select size</option>
+              {this.state.skus.map((sku, index) => (
+                <SelectSize
+                  sku={sku}
+                  key={index}
+                />
+              ))}
+            </select>
+            <select className="selectQuantity" onChange={this.changeQty}>
+              <option selected>Select quantity</option>
+              {this.state.qty.map((qty, index) => (
+                <SelectQuantity
+                  qty={qty}
+                  key={index}
+                />
+              ))}
+            </select>
+            <button className="cartButton" onClick={this.handleClick}>Add to cart</button>
+          </div>
+          <div className="shareButtonsContainer">
+            <div className="shareButtons">
+              <img className="socialImg" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-facebook_-256.png" />
+              <img className="socialImg" src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_instagram-256.png" />
+              <img className="socialImg" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter2_colored_svg-256.png" />
+              <img className="socialImg" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/pinterest-256.png" />
+            </div>
+          </div>
         </div>
-        <img className="shareButtons" src="https://cdn3.iconfinder.com/data/icons/capsocial-round/500/facebook-32.png" />
-        <img className="shareButtons" src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-32.png" />
-        <img className="shareButtons" src="https://cdn-icons-png.flaticon.com/32/174/174863.png" />
-        <div className="stylesContainer">
-          {this.state.styles.results.map((style, index) => (
-            <MoreStyles
-              style={style}
-              key={index}
-              changeStyle={this.changeStyle}
-            />
-          ))}
-        </div>
-        <select className="selectSize" onChange={this.changeSize}>
-          <option selected>Select size</option>
-          {this.state.skus.map((sku, index) => (
-            <SelectSize
-              sku={sku}
-              key={index}
-            />
-          ))}
-        </select>
-        <select className="selectQuantity" onChange={this.changeQty}>
-          <option selected>Select quantity</option>
-          {this.state.qty.map((qty, index) => (
-            <SelectQuantity
-              qty={qty}
-              key={index}
-            />
-          ))}
-        </select>
-        <button className="cartButton" onClick={this.handleClick}>Add to cart</button>
       </div>
     );
   }
--- src/Components/Overview/ProductDetails.jsx
+++ src/Components/Overview/ProductDetails.jsx
@@ -1,39 +1,70 @@
 import React from 'react';
 import StarRatingFetcher from '../RatingsAndReview/StarRatingFetcher.jsx';
 
-const ProductDetails = (props) => {
-
+function ProductDetails(props) {
   function handleClick(e) {
     e.preventDefault();
-    document.getElementById("scroll").scrollIntoView({behavior: "smooth"});
+    document.getElementById('scroll').scrollIntoView({ behavior: 'smooth' });
   }
 
   return props.salePrice ? (
     <>
-    <div className="productStarRating">
-      <StarRatingFetcher productId={props.product.id} />
-    </div>
-    <div className="productReviews" onClick={handleClick}>Read all {props.reviews.length} reviews</div>
-    <div className="productCategory">{props.product.category}</div>
-    <div className="productName">{props.product.name}</div>
-    <div className="productSlogan">{props.product.slogan}</div>
-    <div className="productPriceSale">${props.productPrice}</div>
-    <div className="salePrice">${props.salePrice}</div>
-    <div className="productDescription" id="productDescription">{props.product.description}</div>
-  </>
+      <div className="catRev">
+        <div className="productCategory">{props.product.category}</div>
+        <div className="revs">
+          <div className="productStarRating">
+            <StarRatingFetcher productId={props.product.id} />
+          </div>
+          <div className="productReviews" onClick={handleClick}>
+            Read all
+            {' '}
+            {props.reviews.length}
+            {' '}
+            reviews
+          </div>
+        </div>
+      </div>
+      <div className="nameGang">
+        <div className="productName">{props.product.name}</div>
+        <div className="productSlogan">{props.product.slogan}</div>
+      </div>
+      <div className="productPriceSale">
+        $
+        {props.productPrice}
+      </div>
+      <div className="salePrice">
+        $
+        {props.salePrice}
+      </div>
+      <div className="productDescription" id="productDescription">{props.product.description}</div>
+    </>
   ) : (
-  <>
-    <div className="productStarRating">
-      <StarRatingFetcher productId={props.product.id} />
-    </div>
-    <div className="productReviews"onClick={handleClick}>Read all {props.reviews.length} reviews</div>
-    <div className="productCategory">{props.product.category}</div>
-    <div className="productName">{props.product.name}</div>
-    <div className="productSlogan">{props.product.slogan}</div>
-    <div className="productPriceNoSale">${props.productPrice}</div>
-    <div className="productDescription">{props.product.description}</div>
-  </>
-  )
+    <>
+      <div className="catRev">
+        <div className="productCategory">{props.product.category}</div>
+        <div className="revs">
+          <div className="productStarRating">
+            <StarRatingFetcher productId={props.product.id} />
+          </div>
+          <div className="productReviews" onClick={handleClick}>
+            Read all
+            {props.reviews.length}
+            {' '}
+            reviews
+          </div>
+        </div>
+      </div>
+      <div className="nameGang">
+        <div className="productName">{props.product.name}</div>
+        <div className="productSlogan">{props.product.slogan}</div>
+      </div>
+      <div className="productPriceNoSale">
+        $
+        {props.productPrice}
+      </div>
+      <div className="productDescription">{props.product.description}</div>
+    </>
+  );
 }
 
 export default ProductDetails;
